efb3b5822c6645746d798e876a4e464d82df5f03
AlexanderOkhrimenko <xanderhard@icloud.com>
2020-03-14 20:23:16 +0300

    first commit

---
+++ api/go.mod
@@ -0,0 +1,8 @@
+module go_redis_test
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.5.0
+	github.com/go-redis/redis/v7 v7.2.0 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+"fmt"
+"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+)
+
+func main() {
+
+	router := gin.Default()
+	gin.SetMode(gin.DebugMode)
+
+	// Simple group: v1
+	v1 := router.Group("/v1")
+	{
+		v1.POST("/url.insert", ExampleClient)
+
+	}
+	router.Run(":8080")
+
+}
+
+func ExampleNewClient() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	// Output: PONG <nil>
+}
+
+func ExampleClient(*gin.Context) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	err := client.Set("key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := client.Get("key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := client.Get("key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+}
